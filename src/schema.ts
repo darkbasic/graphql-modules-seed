9ebd5847735b49306871e49c30819b9398d1aaf4
Niccolò Belli <niccolo.belli@linuxsystems.it>
2018-11-03 16:08:28 +0100

    Update to new codegen

---
+++ src/schema.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata';
+import { makeExecutableSchema } from 'graphql-tools';
+import { GraphQLModule } from "@graphql-modules/core";
+import { commonModule } from "@modules/common";
+import { chatsModule } from "@modules/chats";
+import { messagesModule } from "@modules/messages";
+
+const { typeDefs } = new GraphQLModule({
+  name: 'app',
+  imports: [
+    commonModule.forRoot({
+      chats: [],
+      messages: [],
+    }),
+    chatsModule,
+    messagesModule,
+  ],
+});
+export default makeExecutableSchema({
+  typeDefs
+});
