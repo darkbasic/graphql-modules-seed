3293dfe0abb54fc701f23679c9834b5dbdc04e53
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-11-14 03:25:38 -0500

    Remove unnecessary makeSchema
    Signed-off-by: Arda TANRIKULU <ardatanrikulu@gmail.com>

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,5 @@
 import 'reflect-metadata';
-import { makeExecutableSchema } from 'graphql-tools';
 import { AppModule } from '@modules/app';
 
-// Ask for typeDefs without all schema with business logi
-const { typeDefs } = AppModule;
-export default makeExecutableSchema({
-  typeDefs
-});
+// Ask for typeDefs without all schema with business logic
+export default AppModule.typeDefs;
